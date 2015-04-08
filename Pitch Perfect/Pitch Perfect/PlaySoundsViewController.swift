02452221a9c149d58e807c94b60b57d970894dd7
BPayawal <brian.payawal@gmail.com>
2015-04-08 02:02:21 -0700

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Brian M Payawal on 4/6/15.
+//  Copyright (c) 2015 Brian Payawal. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
